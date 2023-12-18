dc5cf2efdab6ee88f7756872f0edaee624ee0d27
Ebby Chakkungal <121340114+vlirkbog@users.noreply.github.com>
2023-12-18 15:57:44 +0100

    ok

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import ButtonComponent from "./components/ButtonComponent";
+import ImageComponent from "./components/ImageComponent";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Esercizio con react</h1>
+      <div className="BtnContainer">
+        <ButtonComponent id="Clicca qui" style={{backgroundColor: "lightBlue", border: "none", padding: "0.5em", margin: "0.5em"}}/>
+        <ButtonComponent id="Click here" style={{backgroundColor: "violet", border: "none", padding: "0.5em", margin: "0.5em"}} />
+      </div>
+      <div className="ImgContainer">
+      <ImageComponent
+        src="https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w1200/2023/10/free-images.jpg"
+        alt="photo"
+        style = {{width: "30em", height: "10em", objectFit: "cover", margin: "3em", borderRadius: "1em"}}
+      />
+      <ImageComponent
+        src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"
+        alt="photo"
+        style = {{width: "30em", height: "10em", objectFit: "cover", margin: "3em", borderRadius: "1em"}}
+      />
+      </div>
     </div>
   );
 }
---
+++ src/components/ButtonComponent.jsx
@@ -0,0 +1,4 @@
+const ButtonComponent = (props) => {
+  return <button style={props.style}>{props.id}</button>;
+};
+export default ButtonComponent;
